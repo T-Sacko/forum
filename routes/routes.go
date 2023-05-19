ac5b2087aab47d68d10e9711dea3f0b2c32eb7fe
titi <sackotiti@gmail.com>
2023-05-19 15:41:51 +0100

    added routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,18 @@
 package routes
+
+import(
+	"net/http"
+)
+
+func RegisterHandler(w *http.ResponseWriter r *http.Request) {
+	err := r.ParseForm()
+	if err != nil    {
+		http.Error(w, "Failed to parseform")
+	}
+
+	email:= r.FormValue("email-address")
+	username:=r.FormValue("username")
+	password:=r.FormValue("password")
+	
+
+}
