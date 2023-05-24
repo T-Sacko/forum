0006da9739ff4310393977dd7a0fb6a2013be13b
Your Name <you@example.com>
2023-05-24 14:45:51 +0100

    Opening Browser Directly

--- main.go
+++ main.go
@@ -2,36 +2,34 @@ package main
 
 import (
 	"fmt"
+	c "forum/controllers"
 	"forum/models"
+	r "forum/routes"
 	"html/template"
 	"log"
 	"net/http"
-	c "forum/controllers"
+	"os/exec"
+
 	_ "github.com/mattn/go-sqlite3"
-	r "forum/routes"
 )
-
-
-type Post struct {
-	ID       int
-	Username string
-	Email    string
-	Content  string
-	Likes    int
-}
-
 
 func init() {
 	c.Tpl = template.Must(template.ParseGlob("templates/*.html"))
 }
 
+func Start() {
+	main()
+}
+
 func main() {
 	models.InitDB()
 	defer models.CloseDB()
-
+	LocalHost := ":8888"
+	url := "https://localhost" +LocalHost
+	exec.Command("open", url).Start()
 	mux := http.NewServeMux()
 	r.SetUpRoutes(mux)
-	err := http.ListenAndServe(":8888", mux)
+	err := http.ListenAndServe(LocalHost, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
