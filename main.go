3acff359892608d8971ee847ae106001bb775538
T-sacko <sackotiti@gmail.com>
2023-05-24 14:47:28 +0100

    db finished

--- main.go
+++ main.go
@@ -6,13 +6,14 @@ import (
 	"forum/models"
 	"forum/routes"
 	"net/http"
+	"os/exec"
 	// _ "github.com/mattn/go-sqlite3"
 	// "golang.org/x/crypto/bcrypt"
 )
 
 func main() {
 	models.InitDB()
-
+	exec.Command("open",)
 	routes.SetUpRoutes()
 
 	err := http.ListenAndServe(":5505", nil)
