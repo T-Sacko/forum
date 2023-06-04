34233ffb5395d81e09e54a3e33c8fb3d121016ab
T-sacko <sackotiti@gmail.com>
2023-06-04 19:38:57 +0100

    yh

--- models/user.go
+++ models/user.go
@@ -1,25 +1,65 @@
 package models
 
+import (
+	"database/sql"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 // func CreateUser(email, username, password string) {
 // 	db.Exec("INSERT INTO users (email, username, password) VALUES (?,?,?)", email, username, password)
 // }
 
-
 type User struct {
-    ID        int       `json:"id"`
-    Email     string    `json:"email"`
-    Username  string    `json:"username"`
-    Password  string    `json:"-"`
-
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Password string `json:"-"`
 }
 
 func (u *User) Save() error {
 
 	_, err := db.Exec("INSERT INTO users (email, username, password) VALUES (?,?,?)", u.Email, u.Username, u.Password)
 	if err != nil {
-	return err
+		return err
 	}
 	return nil
+}
+
+func IsUsernameAvailable(username string) (bool, error) {
+	var count int
+	err := db.QueryRow("SELECT COUNT(*) FROM users WHERE username = ?", username).Scan(&count)
+	if err != nil {
+		return false, err
+	}
+	return count == 0, nil
+}
+
+func IsEmailAvailable(email string) (bool, error) {
+	var count int
+	err := db.QueryRow("SELECT COUNT(*) FROM users WHERE email = ?", email).Scan(&count)
+	if err != nil {
+		return false, err
+	}
+	return count == 0, nil
+}
+
+func ComparePasswords(hashedPassword, userPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(userPassword))
+	return err == nil
+}
+
+func Check4User(email, password string) (bool, error) {
+	var hashedPassword string
+	err := db.QueryRow("SELECT password FROM users WHERE email = ?", email).Scan(&hashedPassword)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false, err
+		} else {
+			return false, err
+		}
+	}
+	return ComparePasswords(hashedPassword, password), nil
 }
 
 // func GetUserByID(id int) (*User, error) {
